b4bfc20bfd0e9a43a65f7d1852092780bd80d8b0
suzhifeng <gdhzyz@163.com>
2024-08-13 07:22:14 +0000

    update bclk calculation script

--- python/i2s/bclk.py
+++ python/i2s/bclk.py
@@ -1,6 +1,6 @@
 
-sr_list = [48, 96] # sample rate, in KHz
-width_list = [24, 32] # data width
+sr_list = [48, 96, 192] # sample rate, in KHz
+width_list = [16, 32] # data width
 tdm_list = [2, 4, 8, 16] # TDM number
 
 mclki = 24.576 # MHz
@@ -12,4 +12,4 @@ for sr in sr_list:
             bclk_mhz = bclk / 1000 /1000
             freq_factor = mclki / bclk_mhz
             if freq_factor >= 1:
-                print(f'mclki {mclki}MHz sample_rate {sr:<2}KHz width {width}b tdm {tdm:>2}, bclk {bclk_mhz: =6.3f}MHz freq_factor {freq_factor: =6.3f}')
+                print(f'mclki {mclki}MHz sample_rate {sr:>3}KHz width {width}b tdm {tdm:>2}, bclk {bclk_mhz: =6.3f}MHz freq_factor {freq_factor: =6.3f}')
